c75933d06c1e823305328a6fd43b21d00ac3a289
Optimus <dns170319@gmail.com>
2024-10-02 21:56:03 +0700

    start pygame

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello World!!!!")
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((700, 700))
+done = False
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    pygame.display.flip()
